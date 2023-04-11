815bd45f3f05414d1d83b5c976e38786b3d85b05
Petr Pechman <petr@petr.pechman>
2023-04-11 14:30:59 +0200

    Add pipelines

---
+++ code/identity/run_identity.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/lnet/aic/opt/cuda/cuda-11.8
+
+source /home/pechmanp/master-thesis/code/czech-gec/venv-envs/.venv/bin/activate
+
+python3 /home/pechmanp/master-thesis/code/czech-gec/identity/identity.py

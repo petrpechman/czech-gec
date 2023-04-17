6f54875afedc48b58b4fcfcea48497efc3c51223
Petr Pechman [NPFL114] <pechmanp@aic.aic.hide.ms.mff.cuni.cz>
2023-04-17 18:25:48 +0200

    add evaluation

---
+++ code/evaluation/run_pipeline.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=/lnet/aic/opt/cuda/cuda-11.8
+
+source /home/pechmanp/DP/czech-gec/code/venv-envs/.venv/bin/activate
+
+python3 /home/pechmanp/DP/czech-gec/code/evaluation/evaluation.py

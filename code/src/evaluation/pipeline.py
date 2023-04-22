23af0e3285339cb3c03473eb30e33974fa191ee1
Petr Pechman <petr@petr.pechman>
2023-04-22 20:22:52 +0200

    Fix loss function

--- code/src/evaluation/pipeline.py
+++ code/src/evaluation/pipeline.py
@@ -79,17 +79,23 @@ with strategy.scope():
 with strategy.scope(): 
     loss = None   
     if config['loss'] == "SCC":
-        def hf_compute_loss(labels, logits):
-            # source: https://github.com/huggingface/transformers/blob/04ab5605fbb4ef207b10bf2772d88c53fc242e83/src/transformers/modeling_tf_utils.py#L210
-            loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(
-                from_logits=True, reduction=tf.keras.losses.Reduction.NONE
-            )
-            unmasked_loss = loss_fn(tf.nn.relu(labels), logits)
-            loss_mask = tf.cast(labels != -100, dtype=unmasked_loss.dtype)
-            masked_loss = unmasked_loss * loss_mask
-            reduced_masked_loss = tf.reduce_sum(masked_loss) / tf.reduce_sum(loss_mask)
-            return tf.reshape(reduced_masked_loss, (1,))
-        loss = hf_compute_loss
+        
+        class MaskedSparseCategoricalCrossEntropy(tf.keras.losses.Loss):
+            def call(self, y_true, y_pred):
+                return self.hf_compute_loss(y_true, y_pred)
+
+            def hf_compute_loss(self, labels, logits):
+                # source: https://github.com/huggingface/transformers/blob/04ab5605fbb4ef207b10bf2772d88c53fc242e83/src/transformers/modeling_tf_utils.py#L210
+                loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(
+                    from_logits=True, reduction=tf.keras.losses.Reduction.NONE
+                )
+                unmasked_loss = loss_fn(tf.nn.relu(labels), logits)
+                loss_mask = tf.cast(labels != -100, dtype=unmasked_loss.dtype)
+                masked_loss = unmasked_loss * loss_mask
+                reduced_masked_loss = tf.reduce_sum(masked_loss) / tf.reduce_sum(loss_mask)
+                return tf.reshape(reduced_masked_loss, (1,))
+        
+        loss = MaskedSparseCategoricalCrossEntropy()
     
 
 # %%

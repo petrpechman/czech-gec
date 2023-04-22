483058a1c4aa8f4fc5a207a75dfaf43a8fb70af5
Petr Pechman [NPFL114] <pechmanp@aic.aic.hide.ms.mff.cuni.cz>
2023-04-22 18:01:23 +0200

    Fix universal script to run experiment.

--- code/src/run_exp.sh
+++ code/src/run_exp.sh
@@ -4,12 +4,8 @@ set -e
 
 pipeline_dir="$1"
 
-cd "./$pipeline_dir"
-
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=/lnet/aic/opt/cuda/cuda-11.8
 
-source "../venv-envs/.vens/bin/activate"
+source "/home/pechmanp/DP/czech-gec/code/venv-envs/.venv/bin/activate"
 
-# source /home/pechmanp/DP/czech-gec/code/venv-envs/.venv/bin/activate
-
-python3 "./$pipeline_dir/pipeline.py"
+python3 "/home/pechmanp/DP/czech-gec/code/src/$pipeline_dir/pipeline.py"
---
+++ code/src/run_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+while getopts "c:g:m:p:" flag
+    do
+             case "${flag}" in
+                    c) CPU=${OPTARG};;
+                    g) GPU=${OPTARG};;
+                    m) MEM=${OPTARG};;
+                    p) PIPELINE=${OPTARG};;
+             esac
+    done
+
+cd "./$PIPELINE"
+
+sbatch -p gpu -c$CPU --gpus=$GPU --mem=${MEM}G ../run_exp.sh $PIPELINE

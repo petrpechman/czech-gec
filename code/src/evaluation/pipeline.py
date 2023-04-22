6ccfa337514c7e965caa6352b7584b01b820da1e
Petr Pechman <petr@petr.pechman>
2023-04-22 20:42:45 +0200

    Fix loss

--- code/src/evaluation/pipeline.py
+++ code/src/evaluation/pipeline.py
@@ -81,19 +81,19 @@ with strategy.scope():
     if config['loss'] == "SCC":
         
         class MaskedSparseCategoricalCrossEntropy(tf.keras.losses.Loss):
-            def call(self, y_true, y_pred):
+            # source: https://github.com/huggingface/transformers/blob/04ab5605fbb4ef207b10bf2772d88c53fc242e83/src/transformers/modeling_tf_utils.py#L210
+            def __init__(self):
+                self.loss_func = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+            
+            def __call__(self, y_true, y_pred, sample_weight=None):
                 return self.hf_compute_loss(y_true, y_pred)
 
             def hf_compute_loss(self, labels, logits):
-                # source: https://github.com/huggingface/transformers/blob/04ab5605fbb4ef207b10bf2772d88c53fc242e83/src/transformers/modeling_tf_utils.py#L210
-                loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(
-                    from_logits=True, reduction=tf.keras.losses.Reduction.NONE
-                )
-                unmasked_loss = loss_fn(tf.nn.relu(labels), logits)
+                unmasked_loss = self.loss_func(tf.nn.relu(labels), logits)
                 loss_mask = tf.cast(labels != -100, dtype=unmasked_loss.dtype)
                 masked_loss = unmasked_loss * loss_mask
                 reduced_masked_loss = tf.reduce_sum(masked_loss) / tf.reduce_sum(loss_mask)
-                return tf.reshape(reduced_masked_loss, (1,))
+                return reduced_masked_loss
         
         loss = MaskedSparseCategoricalCrossEntropy()
     

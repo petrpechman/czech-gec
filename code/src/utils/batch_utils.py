d6e58113f3c8f7dd6a348a03ace55d5638e8d273
Petr Pechman <petr@petr.pechman>
2023-05-08 18:47:56 +0200

    Add bart config

--- code/src/utils/batch_utils.py
+++ code/src/utils/batch_utils.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 from transformers import TFAutoModelForSeq2SeqLM
 from transformers import AutoTokenizer
+from multiprocessing import Process
+from tensorflow.python.client import device_lib 
 
 LINE = "Nebo nevím nějaké specifiské běloruské národní tradice, protože vyrostl jsem ve městě, kde oslává Vánoc neni tak rozšiřena \
 Nebo nevím nějaké specifiské běloruské národní tradice, protože vyrostl jsem ve městě, kde oslává Vánoc neni tak rozšiřena \
@@ -11,10 +13,6 @@ Nebo nevím nějaké specifiské běloruské národní tradice, protože vyrostl
 Nebo nevím nějaké specifiské běloruské národní tradice, protože vyrostl jsem ve městě, kde oslává Vánoc neni tak rozšiřena \
 Nebo nevím nějaké specifiské běloruské národní tradice, protože vyrostl jsem ve městě, kde oslává Vánoc neni tak rozšiřena \
 Nebo nevím nějaké specifiské běloruské národní tradice, protože vyrostl jsem ve městě, kde oslává Vánoc neni tak rozšiřena"
-
-
-tokenizer = AutoTokenizer.from_pretrained("google/mt5-small")
-model = TFAutoModelForSeq2SeqLM.from_pretrained("google/mt5-small")
 
 
 def tokenize_line(line, tokenizer, max_length):
@@ -47,43 +45,62 @@ def split_features_and_labels(input_batch):
     else:
         return features, labels
     
-def try_batch_size(model, dataset, batch_size) -> bool:
-    dataset = dataset.batch(batch_size)
-    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+def try_batch_size(model, tokenizer, lines, batch_size, max_length, lr=0.00001) -> bool:
+    print(device_lib.list_local_devices())
 
-    try:
-        model.fit(dataset, epochs=2, steps_per_epoch=2)
-        return True
-    except:
-        return False
-
-
-def get_batch_size(model, tokenizer, max_length) -> int:
-    lr = 0.00001
-    NUM_LINES = 128
-    MAX_BATCH_SIZE = 2049
-    step = 16
-
-    optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
-    model.compile(optimizer=optimizer)
-
-    lines = [LINE] *  NUM_LINES
     dataset = tf.data.Dataset.from_tensor_slices((lines))
-    # dataset = dataset.map(tokenize_line, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
     dataset = dataset.map(lambda line: tokenize_line(line, tokenizer, max_length), num_parallel_calls=tf.data.experimental.AUTOTUNE)
     dataset = dataset.map(lambda input_dict: ensure_shapes(input_dict, max_length), num_parallel_calls=tf.data.experimental.AUTOTUNE)
     dataset = dataset.map(split_features_and_labels, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
-    for batch_size in range(step, MAX_BATCH_SIZE, step):
-        if not try_batch_size(model, dataset, batch_size):
-            return batch_size - step
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+    model.compile(optimizer=optimizer)
+    model.fit(dataset, epochs=2, steps_per_epoch=2)
+
+
+def get_batch_size(model, tokenizer, max_length) -> int:
+    NUM_LINES = 128
+    MAX_BATCH_SIZE = 2049
+    STEP_BATCH = 16
+
+    lines = [LINE] *  NUM_LINES
+
+    for batch_size in range(STEP_BATCH, MAX_BATCH_SIZE, STEP_BATCH):
+        try:
+            process = Process(target=try_batch_size, args=(model, tokenizer, lines, batch_size, max_length,))
+            process.start()
+            process.join()
+            process.close()
+            print(f"Allowed batch size {batch_size} for max_length {max_length}.")
+        except:
+            return batch_size - STEP_BATCH
+        
+def all_batch_sizes(model, tokenizer):
+    MAX_LENGTH = 16384
+    STEP_LENGTH = 16
+
+    batch_sizes = []
+    for max_length in range(STEP_LENGTH, MAX_LENGTH, STEP_LENGTH):
+        batch_size = get_batch_size(model, tokenizer, max_length)
+        if batch_size == 0:
+            break
+        batch_sizes.append((max_length, batch_size))
+    return batch_sizes
         
 def main():
+    tokenizer = AutoTokenizer.from_pretrained("google/mt5-small")
+    model = TFAutoModelForSeq2SeqLM.from_pretrained("google/mt5-small")
+    filename = "mt5-small-batches.txt"
 
-    max_length = 128
+    batch_sizes = all_batch_sizes(model, tokenizer)
 
-    batch_size = get_batch_size(model, tokenizer, max_length)
-    print(batch_size)
+    with open(filename, "w") as f:
+        f.writelines(batch_sizes)
 
-main()
+    print(batch_sizes)
+
+
+if __name__ == "__main__":
+    main()

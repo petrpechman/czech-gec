c80f8cbbd8cdde1748f6591262adb1ec56487e39
Petr Pechman <petr@petr.pechman>
2023-05-08 11:29:10 +0200

    Add multiprocessing Pool

--- code/src/utils/load_data.py
+++ code/src/utils/load_data.py
@@ -10,6 +10,8 @@ import aspell
 from transformers import AutoTokenizer
 import multiprocessing
 from tokenizers import Tokenizer
+from multiprocessing import Pool
+from multiprocessing import Manager
 
 class GenereteErrorLine():
 
@@ -75,30 +77,33 @@ def data_generator(filename, queue, start_position, end_position, gel: GenereteE
                 f.seek(0) 
                 counter = 0
 
-        
 
-def run_processes(queue: Queue, num_parallel: int, filename: str, file_size: int, gel: GenereteErrorLine, tokenizer, max_length):
+
+def run_processes(queue: Queue, pool: Pool, num_parallel: int, filename: str, file_size: int, gel: GenereteErrorLine, tokenizer, max_length):
     start = random.randint(0, file_size-1)
     process_size = file_size // num_parallel
-    
-    positions = []
+
+    arguments = []
+
     current = start
+    start_position = current
     for i in range(num_parallel):
-        positions.append(current)
         current = (current + process_size) % file_size
-    positions.append(start)
+        end_position = current
+        arguments.append((filename, queue, start_position, end_position, gel, tokenizer, max_length,))
+        start_position = current
+    end_position = start
+    arguments.append((filename, queue, start_position, end_position, gel, tokenizer, max_length,))
 
-    processes = []
-    for i in range(num_parallel):
-        process = Process(target=data_generator, args=(filename, queue, positions[i], positions[i+1], gel, tokenizer, max_length,))
-        process.start()
-        processes.append(process)
-
-    return processes
+    pool.starmap(data_generator, arguments)
+    pool.close()
+    pool.join()
 
 
 def run_proccesses_on_files(queue: Queue, files: List[str], num_parallel: int, gel: GenereteErrorLine, tokenizer, max_length):
     index = 0
+    pool = Pool(num_parallel)
+
     while True:
         file = files[index]
 
@@ -107,9 +112,7 @@ def run_proccesses_on_files(queue: Queue, files: List[str], num_parallel: int, g
                 pass
         file_size = count + 1
         
-        processes = run_processes(queue, num_parallel, file, file_size, gel, tokenizer, max_length)
-        for process in processes:
-            process.join()
+        run_processes(queue, pool, num_parallel, file, file_size, gel, tokenizer, max_length)
 
         index += 1
         if index == len(files):
@@ -135,7 +138,6 @@ def main():
     token_file = "/home/petr/Plocha/DP/czech-gec/code/data/vocabluraries/vocabulary_cs.tsv"
     tokens = introduce_errors.get_token_vocabulary(token_file)
     characters = introduce_errors.get_char_vocabulary(lang)
-    # aspell_speller = aspell.Speller('lang', lang)
     token_err_distribution = [0.7, 0.1, 0.1, 0.1, 0]
     char_err_distribution = [0.25, 0.25, 0.25, 0.25, 0]
     token_err_prob = 0.15
@@ -155,7 +157,9 @@ def main():
 
     random.seed(42) 
 
-    queue = Queue(2 * NUM_PARALLEL)
+    manager = Manager()
+    queue = manager.Queue(2 * NUM_PARALLEL)
+
     gel = GenereteErrorLine(tokens, characters, lang, token_err_distribution, char_err_distribution, token_err_prob, char_err_prob) 
 
     process = Process(target=run_proccesses_on_files, args=(queue, [PATH], NUM_PARALLEL, gel, tokenizer, MAX_LENGTH,))
@@ -176,85 +180,13 @@ def main():
                 "decoder_input_ids": (None, )
             })
 
-    # dataset = dataset.map(ensure_shapes, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     dataset = dataset.map(split_features_and_labels, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     dataset = dataset.bucket_by_sequence_length(
             element_length_func=lambda x, y: tf.shape(x['input_ids'])[0],
             bucket_boundaries=[16, 32, 48, 64, 80, 96, 112],
-            # bucket_batch_sizes=[128, 64, 42, 32, 25, 21, 18, 16]
             bucket_batch_sizes=[1, 1, 1, 1 , 1 , 1 , 1, 1]
     )
     dataset = dataset.prefetch(2)
-
-    # def shuffle_batch():
-    #     buffer_size = 100
-    #     buffer = [] 
-
-    #     max_batch_size = -1
-    #     batch_inputs = []
-    #     batch_attention_masks = []
-    #     batch_labels = []
-    #     batch_size_inputs = 0
-    #     batch_size_labels = 0
-
-    #     while True:
-    #         try:
-    #             dato = queue.get()
-
-    #             if len(buffer) < buffer_size: 
-    #                 buffer.append(dato)
-    #             else:
-    #                 index = random.randint(0, buffer_size - 1)
-    #                 input_ids, attention_mask, labels, decoder_input_ids = buffer[index]
-    #                 buffer[index] = dato    
-
-    #                 if max_batch_size == -1:
-    #                     yield (input_ids, 
-    #                            attention_mask, 
-    #                            labels,
-    #                            decoder_input_ids)
-    #                 else:
-    #                     if (batch_size_inputs + len(input_ids) > max_batch_size) or (batch_size_labels + len(labels) > max_batch_size):
-    #                         yield (tf.ragged.stack(batch_inputs), 
-    #                                tf.ragged.stack(batch_attention_masks), 
-    #                                tf.ragged.stack(batch_labels))
-    #                         batch_inputs = []
-    #                         batch_attention_masks = []
-    #                         batch_labels = []
-    #                         batch_size_inputs = 0
-    #                         batch_size_labels = 0   
-
-    #                     batch_size_inputs += len(input_ids)
-    #                     batch_size_labels += len(labels)
-    #                     batch_inputs.append(input_ids)
-    #                     batch_attention_masks.append(attention_mask)
-    #                     batch_labels.append(labels) 
-
-    #         except queue.Empty:
-    #             pass    
-
-
-    # dataset = tf.data.Dataset.from_generator(
-    #     # lambda: iter(queue.get, None),
-    #     shuffle_batch,
-    #     # output_signature=(
-    #     #     tf.RaggedTensorSpec(shape=(None, None), dtype=tf.int32, ragged_rank=1, row_splits_dtype=tf.int32),
-    #     #     tf.RaggedTensorSpec(shape=(None, None), dtype=tf.int32, ragged_rank=1, row_splits_dtype=tf.int32),
-    #     #     tf.RaggedTensorSpec(shape=(None, None), dtype=tf.int32, ragged_rank=1, row_splits_dtype=tf.int32)
-    #     #     )
-    #     output_types=(tf.int32, tf.int32, tf.int32, tf.int32),
-    #     output_shapes=((None, ),(None, ), (None, ), (None, ))
-    #     )   
-
-
-    # # dataset = dataset.apply(tf.data.experimental.dense_to_ragged_batch(batch_size=4))
-    # # dataset = dataset.ragged_batch(4)
-    # dataset = dataset.bucket_by_sequence_length(
-    #         element_length_func=lambda x, y, z, w: tf.shape(x)[0],
-    #         bucket_boundaries=[16, 32, 48, 64, 80, 96, 112],
-    #         bucket_batch_sizes=[128, 64, 42, 32, 25, 21, 18, 16]
-    # )
-    # dataset = dataset.prefetch(2) # Number of batches to prefetch   
 
     for d in dataset:
         print(d)    

d66f8d8c841f5ba9d90ae119c8dd7f863668a0ee
Petr Pechman <petr@petr.pechman>
2023-05-13 23:38:24 +0200

    add evaluator

---
+++ code/src/transformer/evaluator.py
@@ -0,0 +1,93 @@
+import json
+import tensorflow as tf
+from transformers import TFAutoModelForSeq2SeqLM
+
+with open('config.json') as json_file:
+    config = json.load(json_file)
+    
+SEED = config['seed']
+
+# data loading
+DATA_PATHS = config['data_paths']
+NUM_PARALLEL = config['num_parallel']
+MAX_LENGTH = config['max_length']
+SHUFFLE_BUFFER = config['shuffle_buffer']
+
+# model
+MODEL = config['model']
+
+# optimizer
+OPTIMIZER_NAME = config['optimizer']['name']
+OPTIMIZER_PARAMS = config['optimizer']['params']
+
+# loss
+LOSS = config['loss']
+
+# logs
+LOG_FILE = config['log_file']
+MODEL_CHECKPOINT_PATH = config['model_checkpoint_path']
+
+
+if OPTIMIZER_NAME == 'Adam':
+    optimizer = tf.keras.optimizers.Adam(learning_rate=OPTIMIZER_PARAMS['lr'])
+elif OPTIMIZER_NAME == 'AdamW':
+    optimizer = tf.keras.optimizers.experimental.AdamW(learning_rate=OPTIMIZER_PARAMS['lr'])
+elif OPTIMIZER_NAME == 'Adafactor':
+    optimizer = tf.keras.optimizers.experimental.Adafactor(learning_rate=OPTIMIZER_PARAMS['lr'])
+elif OPTIMIZER_NAME == 'AdaptiveAdam':
+    class LRSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+        def __init__(self, warmup_steps, d_model):
+            self.warmup_steps = tf.cast(warmup_steps, tf.float32)
+            self.d_model = tf.cast(d_model, tf.float32)
+        def __call__(self, step):
+            step = tf.cast(step, tf.float32)
+            lr = (1.0/tf.math.sqrt(self.d_model)) * tf.math.minimum(1.0 / tf.math.sqrt(step), (1.0 / tf.math.sqrt(self.warmup_steps)) * ((1.0 * step) / self.warmup_steps))
+            return lr
+    lr = LRSchedule(OPTIMIZER_PARAMS['warmup_steps'], MAX_LENGTH)
+    beta1 = OPTIMIZER_PARAMS['beta1']
+    beta2 = OPTIMIZER_PARAMS['beta2']
+    epsilon = OPTIMIZER_PARAMS['epsilon']
+    optimizer = tf.keras.optimizers.Adam(
+        learning_rate=lr,
+        beta_1=beta1,
+        beta_2=beta2,
+        epsilon=epsilon)
+    
+
+
+loss = None   
+if LOSS == "SCC":
+    class MaskedSparseCategoricalCrossEntropy(tf.keras.losses.Loss):
+        # source: https://github.com/huggingface/transformers/blob/04ab5605fbb4ef207b10bf2772d88c53fc242e83/src/transformers/modeling_tf_utils.py#L210
+        def __init__(self, reduction=tf.keras.losses.Reduction.NONE, name=None):
+            super().__init__(reduction, name)
+            self.loss_func = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction=reduction)
+        
+        def call(self, y_true, y_pred):
+            return self.hf_compute_loss(y_true, y_pred)
+        def hf_compute_loss(self, labels, logits):
+            unmasked_loss = self.loss_func(tf.nn.relu(labels), logits)
+            loss_mask = tf.cast(labels != -100, dtype=unmasked_loss.dtype)
+            masked_loss = unmasked_loss * loss_mask
+            reduced_masked_loss = tf.reduce_sum(masked_loss) / tf.reduce_sum(loss_mask)
+            return reduced_masked_loss
+    
+    loss = MaskedSparseCategoricalCrossEntropy()
+        
+
+
+model = TFAutoModelForSeq2SeqLM.from_pretrained(MODEL)
+
+if loss:
+    model.compile(optimizer=optimizer, loss=loss)
+else:
+    model.compile(optimizer=optimizer)
+
+sidecar = tf.keras.utils.SidecarEvaluator(
+        model,
+        checkpoint_dir=LOG_FILE,
+        steps=None,
+        max_evaluations=None,
+        callbacks=[]
+    )
+sidecar.start()

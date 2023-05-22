9f953bbd390d0a37e0f0e47419e913e55a21430d
Petr Pechman <petr@petr.pechman>
2023-05-22 15:48:19 +0200

    Add evaluator

--- code/src/transformer/evaluator.py
+++ code/src/transformer/evaluator.py
@@ -1,10 +1,6 @@
-# %%
-import sys
-sys.path.append('..')
-
-
-# %%
 import os
+import time
+import shutil
 import tensorflow as tf
 
 from transformers import TFAutoModelForSeq2SeqLM
@@ -12,25 +8,19 @@ from transformers import AutoTokenizer
 from transformers import AutoConfig
 import json
 
-# from m2scorer.util import paragraphs
-from m2scorer.util import smart_open
 from m2scorer.levenshtein import batch_multi_pre_rec_f1
 from m2scorer.m2scorer import load_annotation
 
 from tensorflow.keras import mixed_precision
 
-from utils import load_data
-from utils import introduce_errors 
 
 def main():
-    # %%
     with open('config-eval.json') as json_file:
         config = json.load(json_file)
     
     SEED = config['seed']
     
     # data loading
-    DATA_PATH = config['data_path']
     M2_DATA = config['m2_data']
     MAX_LENGTH = config['max_length']
     BATCH_SIZE = config['batch_size']
@@ -48,20 +38,21 @@ def main():
     LOSS = config['loss']
     
     # logs
-    LOG_FILE = config['log_file']
-    PROFILE_BATCH = config['profile_batch']
     MODEL_CHECKPOINT_PATH = config['model_checkpoint_path']
+
+    # evaluation
+    MAX_UNCHANGED_WORDS = config['max_unchanged_words']
+    BETA = config['beta']
+    IGNORE_WHITESPACE_CASING = config['ignore_whitespace_casing']
+    VERBOSE = config['verbose']
+    VERY_VERBOSE = config['very_verbose']
     
-    # %%
     tf.random.set_seed(SEED)
     
-    # %%
     tokenizer = AutoTokenizer.from_pretrained(TOKENIZER)
     
     
-    # %%
     # loading of dataset:
-
     def split_features_and_labels(input_batch):
        features = {key: tensor for key, tensor in input_batch.items() if key in ['input_ids', 'attention_mask', 'decoder_input_ids']}
        labels = {key: tensor for key, tensor in input_batch.items() if key in ['labels']}
@@ -74,46 +65,33 @@ def main():
        else:
            return features, labels
         
-    def get_tokenized_sentences(line, gold_line):
+    def get_tokenized_sentences(line):
         line = line.decode('utf-8')
-        gold_line = gold_line.decode('utf-8')
-        tokenized = tokenizer(line, text_target=gold_line, padding='max_length', max_length=MAX_LENGTH, truncation=True, return_tensors="tf")
-        return tokenized['input_ids'], tokenized['attention_mask'], tokenized['labels']
+        tokenized = tokenizer(line, max_length=MAX_LENGTH, truncation=True, return_tensors="tf")
+        return tokenized['input_ids'], tokenized['attention_mask']
 
-    def create_error_line(line, gold_line):
-        input_ids, attention_mask, labels = tf.numpy_function(get_tokenized_sentences, inp=[line, gold_line], Tout=[tf.int32, tf.int32, tf.int32])
-        labels = labels[0]
+    def create_error_line(line):
+        input_ids, attention_mask = tf.numpy_function(get_tokenized_sentences, inp=[line], Tout=[tf.int32, tf.int32])
         dato = {
             'input_ids': input_ids[0],
             'attention_mask': attention_mask[0],
-            'labels': labels[1:],
-            "decoder_input_ids": labels[:-1]
         }
         return dato
 
-    def ensure_shapes(x):
-        return {key: tf.ensure_shape(val, (MAX_LENGTH - 1)) if key in ["labels", "decoder_input_ids"] else tf.ensure_shape(val, (MAX_LENGTH)) for key, val in x.items()}
-    
-    print(M2_DATA)
     dev_source_sentences, dev_gold_edits = load_annotation(M2_DATA)
-    gold_sentences = dev_source_sentences
         
-    dataset =  tf.data.Dataset.from_tensor_slices((dev_source_sentences, gold_sentences))
+    dataset =  tf.data.Dataset.from_tensor_slices((dev_source_sentences))
     dataset = dataset.map(create_error_line, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    dataset = dataset.map(ensure_shapes, num_parallel_calls=tf.data.experimental.AUTOTUNE)
     dataset = dataset.map(split_features_and_labels, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    dataset = dataset.batch(BATCH_SIZE)
+    dataset = dataset.padded_batch(BATCH_SIZE, padded_shapes={'input_ids': [None], 'attention_mask': [None]})
     dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
     
-    # %%
     # policy = mixed_precision.Policy('mixed_float16')
     # mixed_precision.set_global_policy(policy)
     
-    # %%
     strategy = tf.distribute.MirroredStrategy()
     print('Number of devices: %d' % strategy.num_replicas_in_sync)
     
-    # %%
     with strategy.scope():
         if OPTIMIZER_NAME == 'Adam':
             optimizer = tf.keras.optimizers.Adam(learning_rate=OPTIMIZER_PARAMS['lr'])
@@ -150,17 +128,16 @@ def main():
                 def __init__(self, reduction=tf.keras.losses.Reduction.NONE, name=None):
                     super().__init__(reduction, name)
                     self.loss_func = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction=reduction)
-                
+
                 def call(self, y_true, y_pred):
                     return self.hf_compute_loss(y_true, y_pred)
-    
+
                 def hf_compute_loss(self, labels, logits):
                     unmasked_loss = self.loss_func(tf.nn.relu(labels), logits)
                     loss_mask = tf.cast(labels != -100, dtype=unmasked_loss.dtype)
                     masked_loss = unmasked_loss * loss_mask
                     reduced_masked_loss = tf.reduce_sum(masked_loss) / tf.reduce_sum(loss_mask)
-                    return reduced_masked_loss
-            
+                    return reduced_masked_loss     
             loss = MaskedSparseCategoricalCrossEntropy()
 
     with strategy.scope():
@@ -169,29 +146,48 @@ def main():
             model = TFAutoModelForSeq2SeqLM.from_config(config)
         else:
             model = TFAutoModelForSeq2SeqLM.from_pretrained(MODEL)
-        
+
         if loss:
             model.compile(optimizer=optimizer, loss=loss)
         else:
             model.compile(optimizer=optimizer)
     
-    # model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
-    #     filepath=os.path.join(MODEL_CHECKPOINT_PATH, 'ckpt-{epoch}/'),
-    #     save_weights_only=True)
+
+    while True:
+        unevaluated = [f for f in os.listdir(MODEL_CHECKPOINT_PATH) if f.startswith('ckpt')]
+
+        for unevaluated_checkpoint in unevaluated:
+            step = int(unevaluated_checkpoint[5:])
+            result_dir = os.path.join(MODEL_CHECKPOINT_PATH, "results")
+
+            model.load_weights(os.path.join(MODEL_CHECKPOINT_PATH, unevaluated_checkpoint)).expect_partial()
     
-    # model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
-    #     filepath=MODEL_CHECKPOINT_PATH,
-    #     save_weights_only=True)
-    
-    # model.fit(dataset, callbacks=[model_checkpoint], epochs=1)
+            predicted_sentences = []
+
+            for i, batch in enumerate(dataset):
+                print(f"Evaluating {i+1}. batch...") 
+                preds = model.generate(batch['input_ids'])
+                batch_sentences = tokenizer.batch_decode(preds, skip_special_tokens=True)
+                predicted_sentences = predicted_sentences + batch_sentences
+
+            p, r, f1 = batch_multi_pre_rec_f1(predicted_sentences, dev_source_sentences, dev_gold_edits, 
+                                              MAX_UNCHANGED_WORDS, BETA, IGNORE_WHITESPACE_CASING, VERBOSE, VERY_VERBOSE)
+            
+            file_writer = tf.summary.create_file_writer(result_dir)
+            with file_writer.as_default():
+                tf.summary.scalar('epoch_precision', p, step)
+                tf.summary.scalar('epoch_recall', r, step)
+                tf.summary.scalar('epoch_f1', f1, step)
+
+                text = "\n".join(predicted_sentences[0:20])
+                print(text)
+                tf.summary.text("predictions", text, step)
 
 
-    # model.predict(dataset.take(1))
-    model.load_weights(filepath=MODEL_CHECKPOINT_PATH)
-    model.generate(["Neco tu je napsane..."])
+            shutil.rmtree(os.path.join(MODEL_CHECKPOINT_PATH, unevaluated_checkpoint))
 
+        time.sleep(10)
 
-# %%
 if __name__ == '__main__':
     main()
 

cca89a9072b46a55d5b0b3e0fdf634f9e2b835f7
Petr Pechman <petr@petr.pechman>
2023-05-22 16:56:48 +0200

    Add script to sort dev data.

---
+++ code/src/utils/sort_m2.py
@@ -0,0 +1,24 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", required=True)
+    parser.add_argument("--output", required=True)
+    args = parser.parse_args()
+
+    with open(args.input, 'r') as file:
+        content = file.read()
+
+    items = content.split('\n\n')
+    items[-1] = items[-1].strip('\n')
+
+    sorted_items = sorted(items, key=lambda item: len(item.split('\n')[0]), reverse=True)
+
+    sorted_content = '\n\n'.join(sorted_items)
+
+    with open(args.output, 'w') as file:
+        file.write(sorted_content)
+        file.write("\n")
+
+if __name__ == "__main__":
+    main()

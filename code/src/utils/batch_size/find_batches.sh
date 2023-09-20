fcf4ba1913b5d426ecd1592b84abac2381a60711
Petr Pechman [NPFL114] <pechmanp@aic.aic.hide.ms.mff.cuni.cz>
2023-09-20 17:17:17 +0200

    Move batch size scripts

---
+++ code/src/utils/batch_size/find_batches.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define the range of max_length values
+max_lengths=(32 64 96 128)
+
+# Define the starting batch size and step size
+start_batch_size=4
+step_size=4
+max_size=512
+filename="./results/mt5-small-node1.txt"
+config="../../mt5-small/config-small.json"
+text_file="./text.txt"
+
+# Loop over max_length values
+for max_length in "${max_lengths[@]}"; do
+    # Loop over batch sizes
+    for batch_size in $(seq $start_batch_size $step_size $max_size); do
+        output=$(../../../venv-envs/.venv/bin/python3.9 try_batch_size.py --batch-size=$batch_size --max-length=$max_length --output=$filename --config=$config --text=$text_file)
+        
+        # Check if the output is not empty
+        if [ -n "$output" ]; then
+            echo "$output"
+        else
+            echo "Finish fo max length: $max_length"
+            continue
+        fi
+    done
+done
+
+
+# Usage: bash find_batches.sh
---
+++ code/src/utils/batch_size/try_batch_size.py
@@ -0,0 +1,56 @@
+import os
+import argparse
+
+EPOCHS = 2
+STEPS_PER_EPOCH = 4
+
+
+def try_batch_size(batch_size, max_length, epochs, steps_per_epoch, config, text_file) -> bool:
+    import batch_pipeline
+    batch_pipeline.main(batch_size, max_length, epochs, steps_per_epoch, config, text_file)
+
+
+def log_data(filename: str, text: str):
+    if os.path.exists(filename):
+        append_write = 'a'
+    else:
+        append_write = 'w'
+    with open(filename, append_write, encoding="utf-8") as log_file:
+        print(text, file=log_file)
+
+
+def get_batch_size(batch_size: int, max_length, epochs, steps_per_epoch, filename, config, text_file) -> int:
+    try:
+        try_batch_size(batch_size, max_length, epochs, steps_per_epoch, config, text_file)
+        log_data(filename, f"Allowed batch size {batch_size} for max_length {max_length}.")
+        print(f"Allowed batch size {batch_size} for max_length {max_length}.")
+    except Exception as e:
+        print(e)
+        return
+
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--batch-size", type=int)
+    parser.add_argument("--max-length", type=int)
+    parser.add_argument("--output", type=str)
+    parser.add_argument("--config", type=str)
+    parser.add_argument("--text", type=str)
+    args = parser.parse_args()
+
+    
+    # filename = "mt5-small-node1.txt"
+    filename = args.output
+    
+    # config = "../../mt5-small/config-small.json"
+    config = args.config
+
+    # Some text file enough length with enough sentence length for try
+    # text_file = "./text.txt"
+    text_file = args.text
+
+    get_batch_size(args.batch_size, args.max_length, EPOCHS, STEPS_PER_EPOCH, filename, config, text_file)
+
+
+if __name__ == "__main__":
+    main()

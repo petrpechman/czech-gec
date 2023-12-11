f2f02fbbcb8008968b3136e6d251741b21f059a6
Petr Pechman <petr@petr.pechman>
2023-12-11 08:39:22 +0100

    debug

--- code/src/utils/create_errors.py
+++ code/src/utils/create_errors.py
@@ -296,7 +296,7 @@ class ErrorGenerator:
             new_sentence += new_char
         return new_sentence
     
-    def create_error_sentence(self, sentence: str, annotator: Annotator, aspell_speller, use_char_level: bool = False) -> List[str]:
+    def create_error_sentence(self, sentence: str, annotator, aspell_speller, use_char_level: bool = False) -> List[str]:
         # annotator = errant.load('cs')
         try:
             print("BEFORE")

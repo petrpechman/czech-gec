fa938e3f6af4b700ad4424a13ba732c1124f6546
Petr Pechman <petr@petr.pechman>
2024-02-04 18:28:49 +0100

    Add retag

---
+++ code/src/scripts/retag.sh
@@ -0,0 +1,5 @@
+input_path=$1
+# cat ../../data/akces-gec/dev/dev.all.m2  | grep "^..*$" | sed 's|^S|\nS|' | sed 1d > input.m2
+cat ${input_path}  | grep "^..*$" | sed 's|^S|\nS|' | sed 1d > input.m2
+errant_m2 -auto input.m2 -out mid.m2
+retag -i mid.m2 -o out.m2

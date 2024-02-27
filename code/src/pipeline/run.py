c55e502475e9113b45504f447a6fcc23f44bba65
Petr Pechman <petr@petr.pechman>
2024-02-27 14:39:04 +0100

    Add mulit

--- code/src/pipeline/run.py
+++ code/src/pipeline/run.py
@@ -1,10 +1,11 @@
 import argparse
 import pipeline
+import pipeline_multi_gpu
 import evaluator
 import generate_error_data
 import create_dataset
 
-def main(config_filename: str, eval: bool, generate: bool = False, create: bool = False):
+def main(config_filename: str, eval: bool, generate: bool = False, create: bool = False, multi: bool = False):
     if (eval and generate) or (eval and create) or (create and generate):
         print("It is not possible to use eval, generate or create together...")
 
@@ -17,7 +18,10 @@ def main(config_filename: str, eval: bool, generate: bool = False, create: bool 
     elif eval:
         evaluator.main(config_filename)
     else:
-        pipeline.main(config_filename)
+        if multi:
+            pipeline_multi_gpu.main(config_filename)
+        else:
+            pipeline.main(config_filename)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -25,7 +29,8 @@ if __name__ == '__main__':
     parser.add_argument("--eval", action=argparse.BooleanOptionalAction, help="Run evaluation.")
     parser.add_argument("--generate", action=argparse.BooleanOptionalAction, help="Generate by reverted pipeline.")
     parser.add_argument("--create", action=argparse.BooleanOptionalAction, help="Create dataset.")
+    parser.add_argument("--multi", action=argparse.BooleanOptionalAction, help="Multi GPU training.")
     args = parser.parse_args()
 
     print(args.eval)
-    main(args.config, args.eval, args.generate, args.create)
+    main(args.config, args.eval, args.generate, args.create, args.multi)

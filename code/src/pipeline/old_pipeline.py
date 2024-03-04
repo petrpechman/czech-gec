8fa8ead29c3a14344799e006218e40111ce9f4e7
Petr Pechman <petr@petr.pechman>
2024-03-04 08:43:12 +0100

    Add old scripts

--- code/src/pipeline/old_pipeline.py
+++ code/src/pipeline/old_pipeline.py
@@ -204,13 +204,11 @@ def main(config_filename: str):
     dataset = dataset.map(lambda x, y: dataset_utils.change_value(x, y, 0, LABEL_PAD_VALUE, MODEL_TYPE))
     dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
-    # dataset = strategy.experimental_distribute_dataset(dataset)
-    with strategy.scope():
-        if USE_F16:
-            policy = mixed_precision.Policy('mixed_float16')
-            mixed_precision.set_global_policy(policy)
+    if USE_F16:
+        policy = mixed_precision.Policy('mixed_float16')
+        mixed_precision.set_global_policy(policy)
 
-    
+    with strategy.scope():
         ### Optimizer:
         if OPTIMIZER_NAME == 'Adam':
             optimizer = tf.keras.optimizers.Adam(**OPTIMIZER_PARAMS)
@@ -257,55 +255,56 @@ def main(config_filename: str):
             model.compile(optimizer=optimizer)
         ###
 
-        ### Callbacks
-        model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
-            filepath=os.path.join(MODEL_CHECKPOINT_PATH, 'ckpt-{epoch}/'),
-            save_weights_only=True,
-            save_freq="epoch")
+    ### Callbacks
+    model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
+        filepath=os.path.join(MODEL_CHECKPOINT_PATH, 'ckpt-{epoch}/'),
+        save_weights_only=True,
+        save_freq="epoch")
+    
+    model_checkpoint_optimizer = MyBackupAndRestore(
+        os.path.join(MODEL_CHECKPOINT_PATH, "optimizer"), optimizer, model,
+        epoch_name="opt-ckpt",
+        max_to_keep=None,
+    )
 
-        model_checkpoint_optimizer = MyBackupAndRestore(
-            os.path.join(MODEL_CHECKPOINT_PATH, "optimizer"), optimizer, model,
-            epoch_name="opt-ckpt",
-            max_to_keep=None,
-        )
+    mybackup = MyBackupAndRestore(BACKUP_DIR, optimizer, model, max_to_keep=1)
+    status = mybackup.checkpoint.restore(mybackup.manager.latest_checkpoint)
+    status_optimizer = model_checkpoint_optimizer.checkpoint.restore(mybackup.manager.latest_checkpoint)
+    print("STATUS:", status)
+    initial_epoch = mybackup._ckpt_saved_epoch
+    print("INITIAL EPOCH:", int(initial_epoch))
 
-        mybackup = MyBackupAndRestore(BACKUP_DIR, optimizer, model, max_to_keep=1)
-        status = mybackup.checkpoint.restore(mybackup.manager.latest_checkpoint)
-        status_optimizer = model_checkpoint_optimizer.checkpoint.restore(mybackup.manager.latest_checkpoint)
-        
+    profiler = tf.keras.callbacks.TensorBoard(
+        log_dir=LOG_FILE, 
+        profile_batch=PROFILE_BATCH)
 
-        profiler = tf.keras.callbacks.TensorBoard(
-            log_dir=LOG_FILE, 
-            profile_batch=PROFILE_BATCH)
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=LOG_FILE, 
+        histogram_freq=1)
 
-        tensorboard_callback = tf.keras.callbacks.TensorBoard(
-            log_dir=LOG_FILE, 
-            histogram_freq=1)
+    callbacks = [
+        model_checkpoint,
+        mybackup,
+        model_checkpoint_optimizer,
+        profiler,
+        tensorboard_callback
+    ]
+    ###
 
-        callbacks = [
-            model_checkpoint,
-            mybackup,
-            model_checkpoint_optimizer,
-            profiler,
-            tensorboard_callback
-        ]
-        ###
+    if LR:
+        print("LEARNING RATE:")
+        print(LR)
+        optimizer.learning_rate = tf.Variable(LR)
+        optimizer._learning_rate = tf.Variable(LR)
+        print(optimizer.learning_rate)
+        print(optimizer._learning_rate)
+        print("--------------")
 
-        if LR:
-            print("LEARNING RATE:")
-            print(LR)
-            optimizer.learning_rate = tf.Variable(LR)
-            optimizer._learning_rate = tf.Variable(LR)
-            print(optimizer.learning_rate)
-            print(optimizer._learning_rate)
-            print("--------------")
+    ### Train
+    if USE_F16 and MODEL_TYPE == "Bart-mine":
+        model.model.encoder.embed_scale = tf.cast(model.model.encoder.embed_scale, tf.float16)
+        model.model.decoder.embed_scale = tf.cast(model.model.decoder.embed_scale, tf.float16)
 
-        ### Train
-        if USE_F16 and MODEL_TYPE == "Bart-mine":
-            model.model.encoder.embed_scale = tf.cast(model.model.encoder.embed_scale, tf.float16)
-            model.model.decoder.embed_scale = tf.cast(model.model.decoder.embed_scale, tf.float16)
-
-    initial_epoch = "0"
     if STEPS_PER_EPOCH:
         model.fit(
             dataset, 
--- code/src/pipeline/run.py
+++ code/src/pipeline/run.py
@@ -1,11 +1,13 @@
 import argparse
 import pipeline
-import pipeline_multi_gpu
 import evaluator
 import generate_error_data
 import create_dataset
 
-def main(config_filename: str, eval: bool, generate: bool = False, create: bool = False, multi: bool = False):
+import old_pipeline
+import old_evaluator
+
+def main(config_filename: str, eval: bool, generate: bool = False, create: bool = False, old: bool = False):
     if (eval and generate) or (eval and create) or (create and generate):
         print("It is not possible to use eval, generate or create together...")
 
@@ -16,10 +18,13 @@ def main(config_filename: str, eval: bool, generate: bool = False, create: bool 
         print("USE CREATE...")
         create_dataset.main(config_filename)
     elif eval:
-        evaluator.main(config_filename)
+        if args.old:
+            old_evaluator.main(config_filename)
+        else:
+            evaluator.main(config_filename)
     else:
-        if multi:
-            pipeline_multi_gpu.main(config_filename)
+        if args.old:
+            old_pipeline.main(config_filename)
         else:
             pipeline.main(config_filename)
 
@@ -29,8 +34,8 @@ if __name__ == '__main__':
     parser.add_argument("--eval", action=argparse.BooleanOptionalAction, help="Run evaluation.")
     parser.add_argument("--generate", action=argparse.BooleanOptionalAction, help="Generate by reverted pipeline.")
     parser.add_argument("--create", action=argparse.BooleanOptionalAction, help="Create dataset.")
-    parser.add_argument("--multi", action=argparse.BooleanOptionalAction, help="Multi GPU training.")
+    parser.add_argument("--old", action=argparse.BooleanOptionalAction, help="Use old code...")
     args = parser.parse_args()
 
     print(args.eval)
-    main(args.config, args.eval, args.generate, args.create, args.multi)
+    main(args.config, args.eval, args.generate, args.create, args.old)

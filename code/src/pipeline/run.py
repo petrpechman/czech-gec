d76267f1c1499cb33a92b7744ae8d7fa0269ea99
Petr Pechman <petr@petr.pechman>
2024-03-05 08:47:36 +0100

    Try options

--- code/src/pipeline/run.py
+++ code/src/pipeline/run.py
@@ -7,7 +7,12 @@ import create_dataset
 import old_pipeline
 import old_evaluator
 
-def main(config_filename: str, eval: bool, generate: bool = False, create: bool = False, old: bool = False):
+import pipeline_v1
+import pipeline_v2
+import pipeline_v3
+
+def main(config_filename: str, eval: bool, generate: bool = False, create: bool = False, old: bool = False,
+         version: int = 0):
     if (eval and generate) or (eval and create) or (create and generate):
         print("It is not possible to use eval, generate or create together...")
 
@@ -18,15 +23,22 @@ def main(config_filename: str, eval: bool, generate: bool = False, create: bool 
         print("USE CREATE...")
         create_dataset.main(config_filename)
     elif eval:
-        if args.old:
+        if old:
             old_evaluator.main(config_filename)
         else:
             evaluator.main(config_filename)
     else:
-        if args.old:
+        if old:
             old_pipeline.main(config_filename)
         else:
-            pipeline.main(config_filename)
+            if version == 0:
+                pipeline.main(config_filename)
+            elif version == 1:
+                pipeline_v1.main(config_filename)
+            elif version == 2:
+                pipeline_v2.main(config_filename)
+            elif version == 3:
+                pipeline_v3.main(config_filename)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -35,7 +47,8 @@ if __name__ == '__main__':
     parser.add_argument("--generate", action=argparse.BooleanOptionalAction, help="Generate by reverted pipeline.")
     parser.add_argument("--create", action=argparse.BooleanOptionalAction, help="Create dataset.")
     parser.add_argument("--old", action=argparse.BooleanOptionalAction, help="Use old code...")
+    parser.add_argument("--version", type=int, help="Pipeline version.", default=0)
     args = parser.parse_args()
 
     print(args.eval)
-    main(args.config, args.eval, args.generate, args.create, args.old)
+    main(args.config, args.eval, args.generate, args.create, args.old, args.version)

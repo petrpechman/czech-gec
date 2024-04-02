7097538d845cba7528bf7025c53e141de41c0e68
Petr Pechman <petr@petr.pechman>
2024-04-02 13:55:25 +0200

    Add parts

--- code/src/pipeline/run.py
+++ code/src/pipeline/run.py
@@ -6,7 +6,11 @@ import create_dataset
 
 import pipeline_print_output
 
-def main(config_filename: str, eval: bool, generate: bool = False, create: bool = False, print_flag: bool = False):
+import evaluator_pred
+import evaluator_files
+
+def main(config_filename: str, eval: bool, generate: bool = False, create: bool = False, print_flag: bool = False,
+         part2: bool = False, part1: bool = False):
     if (eval and generate) or (eval and create) or (create and generate):
         print("It is not possible to use eval, generate or create together...")
 
@@ -17,7 +21,12 @@ def main(config_filename: str, eval: bool, generate: bool = False, create: bool 
         print("USE CREATE...")
         create_dataset.main(config_filename)
     elif eval:
-        evaluator.main(config_filename)
+        if part1:
+            evaluator_pred.main(config_filename)
+        elif part2:
+            evaluator_files.main(config_filename)
+        else:
+            evaluator.main(config_filename)
     else:
         if print_flag:
             pipeline_print_output.main(config_filename)
@@ -28,10 +37,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--config", type=str, help="Config file.")
     parser.add_argument("--eval", action=argparse.BooleanOptionalAction, help="Run evaluation.")
+    parser.add_argument("--part1", action=argparse.BooleanOptionalAction, help="")
+    parser.add_argument("--part2", action=argparse.BooleanOptionalAction, help="")
     parser.add_argument("--generate", action=argparse.BooleanOptionalAction, help="Generate by reverted pipeline.")
     parser.add_argument("--create", action=argparse.BooleanOptionalAction, help="Create dataset.")
     parser.add_argument("--print", action=argparse.BooleanOptionalAction, help="Print dataset.")
     args = parser.parse_args()
 
     print(args.eval)
-    main(args.config, args.eval, args.generate, args.create, args.print)
+    main(args.config, args.eval, args.generate, args.create, args.print, args.part1, args.part2)

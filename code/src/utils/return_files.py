6618555bc5ec5e0c9434cefa8ed3813f705b54b6
Petr Pechman <petr@petr.pechman>
2024-04-04 13:49:34 +0200

    Add script

---
+++ code/src/utils/return_files.py
@@ -0,0 +1,12 @@
+import os
+
+current_dir = os.getcwd()
+filenames = os.listdir(current_dir)
+filenames = [filename for filename in filenames if not filename.startswith('ckpt-')]
+filenames = [filename for filename in filenames if filename.split('-')[-1].isdigit()]
+
+for filename in filenames:
+    number = filename.split('-')[-1]
+    last_index = filename.rfind('-')
+    new_name = number + '-' + filename[:last_index+1]
+    print(new_name)
